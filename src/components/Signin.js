69afb2e9e5fb15863cd481cb7b80e6e14888dec6
Gamingapple <madhikarmianshu@gmail.com>
2025-03-29 20:57:17 +1100

    Implemented Google OAuth

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,65 +1,79 @@
-import './Signin.css'
-import {signInWithEmailAndPassword} from 'firebase/auth'
-import React from 'react'
-import { auth } from '../config/firebase'
-import { useNavigate } from 'react-router'
+import "./Signin.css";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React from "react";
+import { auth } from "../config/firebase";
+import { useNavigate } from "react-router";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import googlelogo from "../images/google_logo.png";
 
+// Create a Google Authentication Provider
+const provider = new GoogleAuthProvider();
 
+function Signin(props) {
+  const [user, setUser] = React.useState("");
+  const [signnedIn, setSignnedIn] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState(null);
 
-function Signin(props){
-    const [email, setEmail] = React.useState('')
-    const [password, setPassword] = React.useState('')
-    const [errorMessage, setErrorMessage] = React.useState(null)
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  // Handle Google sign-in
+  const GoogleSign = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // Access Google Access Token and user information
+        console.log("WORKING")
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        setUser(result.user);
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Google sign-in errors
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
 
-    const FirebaseSignIn = async ()=>{
-        try{
-          await signInWithEmailAndPassword(auth, email, password)
-          // Perform login logic
-          // Save user data to local storage
-          localStorage.setItem('userEmail', email);
-          navigate("/products")
-        }
-        catch(e){
-            setErrorMessage(e.message)
-            document.querySelector('.password-error').classList.add('error-animate-down')
-            setTimeout(()=>{
-              document.querySelector('.password-error').classList.add('error-animate-up')
-            }, 800)
-            setTimeout(()=>{
-              document.querySelector('.password-error').classList.remove('error-animate-down')
-              document.querySelector('.password-error').classList.remove('error-animate-up')
-            },1500)
-          }
-        }
+  const SignnedIn = () => {
+      console.log(user)
+      setSignnedIn(true);
+      setTimeout(() => setSignnedIn(false), 2000);
 
-    const NavigateSignUp = ()=>{
-        navigate("/signup")
-    }
-
-
-    return(
-        <div>
-                <div className="form-container slidefade animate-slidefade z-applicable">
-                {errorMessage &&<div className="password-error">
-            Error: {errorMessage}
-        </div>}
-      <h3 className="title">Sign In</h3>
-      <form action="">
-          <input type="email" placeholder="Email" name="" id="" onChange={(e)=>setEmail(e.target.value)}/> 
-          <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
-          <br></br>
-          <button className="form-button" type="button" onClick={FirebaseSignIn} name="Hover">Sign-In</button>
-          <br></br>
-          <button className="form-button" type="button" onClick={NavigateSignUp} name="Hover">Create Account</button>
-       </form>
-    </div>
-    <br></br>
-    <br></br>
-    <br></br>
-        </div>
-    )
   }
-  
-  export default Signin
+
+  return (
+    <div>
+      <div className="form-container slidefade animate-slidefade z-applicable">
+        {errorMessage && (
+          <div className="password-error">Error: {errorMessage}</div>
+        )}
+        <h3 className="title">Sign In</h3>
+        <form className="signin-form" action="">
+        <div className="google-btn" onClick={user ? SignnedIn : GoogleSign}>
+          <div className="google-icon-wrapper">
+            <img
+              className="google-icon"
+              src={googlelogo}
+              alt="Google Icon"
+            />
+          </div>
+          <p className="btn-text">
+            <b>{user.displayName}</b>
+          </p>
+        </div>
+        <p className="siggnedInMessage">{signnedIn ? "User Already Signned In" : ""}</p>
+
+        </form>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+  );
+}
+
+export default Signin;
